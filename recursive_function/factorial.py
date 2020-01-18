a4e28e6377395376e619cd59792bd8f8aa0db29a
leebomin <codavincee@gmail.com>
2020-01-18 22:52:33 +0900

    Factorial solved with second solution

--- recursive_function/factorial.py
+++ recursive_function/factorial.py
@@ -1,5 +1,6 @@
 # 1부터 n까지 연속한 정수의 곱을 구하는 알고리즘을 만들어보세요.
 
+# solution 1 for문을 이용한 풀이
 def get_factorial(n):
     f = 1
 
@@ -8,8 +9,19 @@ def get_factorial(n):
 
     return f
 
+# solution 2 재귀함수를 이용한 풀이
+def get_factorial_by_recursive(n):
+    if n == 1:
+        return 1
+    return n * get_factorial_by_recursive(n-1)
+
 
 def test_get_factorial():
     assert get_factorial(1) == 1
     assert get_factorial(2) == 2
     assert get_factorial(5) == 120
+
+def test_get_factorial_by_recursive():
+    assert get_factorial_by_recursive(1) == 1
+    assert get_factorial_by_recursive(2) == 2
+    assert get_factorial_by_recursive(5) == 120
